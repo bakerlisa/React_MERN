0de949957a527fcb910ccf0edbd6181cdf5bd003
Lisa <lb.broadhead@gmail.com>
2022-03-06 15:58:31 -0600

    deconstruction

---
+++ fundamentals/deconstruction.js
@@ -0,0 +1,55 @@
+// 1. PREDICTION: Telsa,Mercedes, OUTPUT: Telsa,Mercedes
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// 2. PREDICTION: unefined,Elon, OUTPUT: error
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// 3. PREDICTION: 12345,Phil Smith, OUTPUT: 12345, undefined
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// 4. PREDICTION: 5,2; false, true OUTPUT: false, true
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+// 2 == 5
+console.log(first == third);
+// 2 == 2
+
+// 5. PREDICTION: lastTest, [1, 5, 1, 8, 3, 3], 1, 5  OUTPUT: value, [1, 5, 1, 8, 3, 3], 1, 5  5 
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
--- fundamentals/tempCodeRunnerFile.js
+++ fundamentals/tempCodeRunnerFile.js
@@ -1,6 +1,12 @@
-function rewind() {
-    genre = "rock";
-    console.log(genre);
-    var genre = "r&b";
-    console.log(genre);
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
 }
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
