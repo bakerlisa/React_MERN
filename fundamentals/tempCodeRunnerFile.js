dda8f86fc3645e03b9812ba18496fccbcd82fb53
Lisa <lb.broadhead@gmail.com>
2022-03-06 16:16:44 -0600

    commit to arrow functions

--- fundamentals/tempCodeRunnerFile.js
+++ fundamentals/tempCodeRunnerFile.js
@@ -1,12 +1,5 @@
-const lastTest = {
-    key: 'value',
-    secondKey: [1, 5, 1, 8, 3, 3]
-}
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-//Predict the output
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
+var sayHello = function(name) {
+    console.log('Hello ' + name);
+  };
+
+  sayHello()
