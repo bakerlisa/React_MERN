17b3dcfe015135e0b76d846fd4230adc4bf00017
Lisa <lb.broadhead@gmail.com>
2022-03-07 10:09:17 -0600

    algos

--- algos/week1/bubblesort.js
+++ algos/week1/bubblesort.js
@@ -6,7 +6,6 @@ function bubbleSort(arr){
     i=0;
     var finish = true;
     while(i < arr.length - 1){
-        console.log()
         if(arr[i] > arr[i+1]){
             var temp = arr[i]
             finish = false;
@@ -43,5 +42,46 @@ function bubbleSort(arr){
     return arr
 }
 
-let arg = [5,4,2,6,8,14,3,1];
-console.log(bubbleSort(arg));
+// let arg = [5,4,2,6,8,14,3,1];
+// console.log(bubbleSort(arg));
+
+function selectionSort(arr,value){
+    var checker = true;
+    var runner = 0;
+
+    while(checker){
+        var checker = false
+        for(i=0;i < arr.length - runner; i++){
+            if(value == arr[i]){
+                checker = true
+                var temp = arr[0];
+                runner++;
+            }    
+            if(arr[i] < arr[i+1]){
+
+            }
+        }
+    }
+    return arr;
+}
+
+// let arg = [5,4,2,6,8,14,3,1];
+// console.log(selectionSort(arg,1));
+
+function selectionSort(arr){
+    for(i=0;i<arr.length;i++){
+        var mindex = i;
+        for(var j = i; j<arr.length;j++){
+            if(arr[j] < arr[mindex]){
+                mindex = j;
+            }
+        }
+        var temp = arr[i];
+        arr[i] = arr[mindex]
+        arr[mindex] = temp
+    }
+    return arr;
+}
+
+let arg = [5,4,2,6,8,14,3,1,-1];
+console.log(selectionSort(arg));
