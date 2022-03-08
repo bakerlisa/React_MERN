4ffedab790dbe04e27a71bcef8bd69fe2231e5c3
Lisa <lb.broadhead@gmail.com>
2022-03-08 10:35:56 -0600

    algos

---
+++ algos/week1/insertion_sorting.js
@@ -0,0 +1,79 @@
+// for(i=1;i < arr.length;i++){ - loop through whole array
+//     // we don't need to start at z cause 1 is always sorted
+//         // IF STATEMENT: compare to values - current, and pervious: 
+//             // #2 FOOR LOOP: --j : 
+//                 // comparing our "new" view value based keep j, other dials backwards
+//                     // IF TRUE: temp to swap
+//  RETURN arr;
+        
+// }
+
+
+// WITH GROUP:
+const insertionSort = (arr) => {
+    var current;
+    for(var i=1;i < arr.length;i++){
+        current = arr[i];
+        for(var j=i - 1;j >= 0 && arr[j] > current; j--){
+            arr[j+1] = arr[j];            
+        }
+        arr[j+1] = current;
+    }
+    return arr;
+}
+// console.log(insertionSort([6,4,5,2,8,14,1,3]))
+
+
+// MIKE:
+
+
+//LISA LOOP:
+const insertionSortLisa = (arr) => {
+    for(var i=1;i < arr.length;i++){
+        var setI = arr[i];
+        for(var j=i;j > 0; j--){
+            // console.log(`i: ${i} j: ${j}`)
+            console.log(`j-1: ${arr[j-1]} setI: ${setI}`)
+
+            if(arr[j-1] > setI){
+                var temp = arr[j]
+                arr[j] = setI;
+                setI = temp
+               // arr[j] = setI;
+                console.log("here")
+
+            } 
+        }
+        console.log();
+        if(i == 2){
+            break;
+        }
+    }
+    return arr;
+}
+console.log(insertionSortLisa([6,4,5,2,8,14,1,3]))
+
+
+
+
+
+
+
+// =========== CODE EDITS:
+// for(i=1;i < arr.length;i++){
+//     // we don't need to start at z cause 1 numbers always sorted
+//         // if compare to values: current, and pervious: 
+//             //anonther foor loop based on 0 to i: -- 
+//                 // if statment to check
+//                     // temp to swap    
+// }
+
+
+// I was doing a swap, he had the if statement in the for loop conditional
+// console.log(`i: ${i} `)
+// console.log()
+// if(arr[j] > arr[i]){
+//     var temp = arr[i];
+//     arr[i] = arr[];
+//     arr[j] = temp;
+// }
