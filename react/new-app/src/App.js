bde0b1e546247d43f74d0c065a6ce662d1a76990
Lisa <lb.broadhead@gmail.com>
2022-03-08 13:39:42 -0600

    new app

--- react/new-app/src/App.js
+++ react/new-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+import NewComponent from './components/newComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewComponent title="this is my title for the page"/>
+      <NewComponent title="Here's another title"/>
+      <NewComponent title="WOW this is awesome!!!"/>
     </div>
   );
 }
