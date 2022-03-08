e48c1cd62e963dec4f3c2be00fc3a788b6941677
Lisa <lb.broadhead@gmail.com>
2022-03-08 14:46:55 -0600

    new app

--- react/prop-it-up/src/App.js
+++ react/prop-it-up/src/App.js
@@ -10,7 +10,9 @@ function App() {
   return (
     <div className="App flex">
       <h1>Welcome Authors:</h1>
-      <PersonalCard firstName="Emily" lastName="Dickinson" age="149" hairColor="Red" image={EmilyDickinson}/>
+      <PersonalCard firstName="Emily" lastName="Dickinson" age="149" hairColor="Red" image={EmilyDickinson}>
+        <p className="sidenote">Sidenote: I'm a child</p>  
+      </PersonalCard>
       <PersonalCard firstName="Jane" lastName="Eyre" age="175" hairColor="Auburn" image={JaneEyre} />
       <PersonalCard firstName="Jane" lastName="Austen" age="247" hairColor="Dark Brown" image={JaneAustin} />
       <PersonalCard firstName="Ayn" lastName="Rand" age="130" hairColor="Brown" image={AynRand} />
--- react/prop-it-up/src/components/PersonalCard.js
+++ react/prop-it-up/src/components/PersonalCard.js
@@ -9,6 +9,7 @@ class PersonalCard extends Component{
                 <h2><span>Full Name: </span>{this.props.firstName} {this.props.lastName}</h2>
                 <p><strong>Age</strong>: { this.props.age }</p>
                 <p><strong>Hair Color</strong>: { this.props.hairColor }</p>
+                {this.props.children}
             </div>
         )
     }
