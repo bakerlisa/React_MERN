e0e8f0e6aa3887e0e16d50e4baa17231c6569604
Lisa <lb.broadhead@gmail.com>
2022-03-08 14:29:37 -0600

    new app

--- react/prop-it-up/src/App.js
+++ react/prop-it-up/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import AynRand from "./img/Ayn_Rand.jpeg";
+import JaneAustin from "./img/jane-austen.jpeg";
+import EmilyDickinson from "./img/emily-dickinson.jpeg";
+import JaneEyre from "./img/jane-eyre.jpeg";
 
 import PersonalCard from './components/PersonalCard';
 
@@ -6,10 +10,10 @@ function App() {
   return (
     <div className="App flex">
       <h1>Welcome Authors:</h1>
-      <PersonalCard firstName="Emily" lastName="Dickinson" age="149" hairColor="Red" image="img/emily-dickinson.jpeg"/>
-      <PersonalCard firstName="Jane" lastName="Eyre" age="175" hairColor="Auburn" image="img/jane-eyre.jpeg"/>
-      <PersonalCard firstName="Jane" lastName="Austen" age="247" hairColor="Dark Brown" image="img/jane-austen.jpeg"/>
-      <PersonalCard firstName="Ayn" lastName="Rand" age="130" hairColor="Brown" image="img/Ayn_Rand.jpeg"/>
+      <PersonalCard firstName="Emily" lastName="Dickinson" age="149" hairColor="Red" image={EmilyDickinson}/>
+      <PersonalCard firstName="Jane" lastName="Eyre" age="175" hairColor="Auburn" image={JaneEyre} />
+      <PersonalCard firstName="Jane" lastName="Austen" age="247" hairColor="Dark Brown" image={JaneAustin} />
+      <PersonalCard firstName="Ayn" lastName="Rand" age="130" hairColor="Brown" image={AynRand} />
     </div>
   );
 }
--- react/prop-it-up/src/components/PersonalCard.js
+++ react/prop-it-up/src/components/PersonalCard.js
@@ -6,8 +6,6 @@ class PersonalCard extends Component{
         return(
             <div className="info-wrapper">
                 <div className="img-wrapper"><img src={this.props.image} alt="image of author " /></div>
-                <div className="img-wrapper"><img src="img/{this.props.image}" alt="image of author " /></div>
-
                 <h2><span>Full Name: </span>{this.props.firstName} {this.props.lastName}</h2>
                 <p><strong>Age</strong>: { this.props.age }</p>
                 <p><strong>Hair Color</strong>: { this.props.hairColor }</p>
