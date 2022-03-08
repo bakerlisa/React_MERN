20cd6f3bc20eb7bead3cdb7cb1870669b1ec5f55
Lisa <lb.broadhead@gmail.com>
2022-03-07 22:30:32 -0600

    jsx react

---
+++ react/first_jsx/first_project/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Clim Mt. Everst</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog!</li>
+        <li>Sleep 12 hours</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
