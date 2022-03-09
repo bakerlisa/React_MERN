e1f67bcf5e650587c07e9c0081da376c10ddb3fa
Lisa <lb.broadhead@gmail.com>
2022-03-09 14:24:49 -0600

    functional components

--- react/prop-up-functional-components/src/components/Books.js
+++ react/prop-up-functional-components/src/components/Books.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import BooksStyles from './css/BooksStyles';
 import styles from './css/BooksStyles.module.css';
 
 
 const Books = props => {
+    const[state,setState] = useState({
+        clickCount: props.votes
+    });
+    const countUp = e => {
+        console.log(state)
+        setState({
+            clickCount: state.clickCount + 1
+        })
+    }
     return(
         <BooksStyles>
             <img src={ props.img } alt="image of books: { props.title }" />
             <h2>{ props.title }</h2>
             <p className="author">{ props.author }</p>
-            <p>Total Votes: { props.votes }</p>
+            <p>Total Votes: { state.clickCount}</p>
+            <button onClick={countUp}>Cast Your Vote!</button>
         </BooksStyles>
     );
 }
