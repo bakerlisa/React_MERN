f3ffbcfdac8cad0a7a993a35486d511f485d16a9
Lisa <lb.broadhead@gmail.com>
2022-03-09 18:28:20 -0600

    more forms and validations

--- react/more-forms/src/components/User.js
+++ react/more-forms/src/components/User.js
@@ -8,10 +8,21 @@ const User = (props) => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    const [errorTitle, setErrorTitle] = useState("");
 
     const createUser = (e) => {
         const newUser = { firstName,lastName,email,password,confirmPassword,hasBeenSubmitted };
         console.log("hello World");
+    }
+
+    const hadleFirstName = (e) => {
+        setFirstName(e.target.value)
+        console.log(firstName.length)
+        if(firstName.length + 1 < 3){
+            setErrorTitle("First Name is required and must be at least 3 characters long")
+        }else{
+            setErrorTitle("✔️")
+        }
     }
 
     return(
@@ -22,23 +33,26 @@ const User = (props) => {
             }
             <div>
                 <label htmlFor="firstName"> Eman Tsrif: </label>
-                <input type="text" name="firstName"/>
+                <input type="text" name="firstName" onChange={ hadleFirstName } />
+                {
+                    errorTitle ? <p className="validations">{ errorTitle }</p> : '' 
+                }
             </div>
             <div>
                 <label htmlFor="lastName"> Eman Tsal: </label>
-                <input type="text" name="lastName"/>
+                <input type="text" name="lastName" onChange={ (e) => setLastName.target.value } />
             </div>
             <div>
                 <label htmlFor="email">Liame: </label>
-                <input type="text" name="email"/>
+                <input type="text" name="email" onChange={ (e) => setEmail.target.value} />
             </div>
             <div>
                 <label htmlFor="password"> Drowssap: </label>
-                <input type="text" name="password"/>
+                <input type="text" name="password" onChange={ (e) => password.target.value } />
             </div>
             <div>
                 <label htmlFor="confirmPassword"> Mrifnoc Drowssap: </label>
-                <input type="text" name="confirmPassword"/>
+                <input type="text" name="confirmPassword" onChange={ (e) => confirmPassword.target.value } />
             </div>
             <div className="submitWrp">
                 <input type="submit" value="Create User"/>
