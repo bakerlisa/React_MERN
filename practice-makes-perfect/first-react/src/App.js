3b5c5b043bc97df3f003c1a93ae22a4543555e9b
Lisa <lb.broadhead@gmail.com>
2022-03-10 21:01:13 -0600

    practice

--- practice-makes-perfect/first-react/src/App.js
+++ practice-makes-perfect/first-react/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import UserCard from './components/UserCard';
-import UserCardFunctional from './components/UserCardFunctional';
+
 
 function App() {
   
   return (
     <div className="App">
-      <UserCard name="lisa" age="15" color="red"/>
-      <UserCardFunctional name="Lisa" age="54" color="black"/>
+      <h1>Hello Wolrd</h1>
     </div>
   );
 }
