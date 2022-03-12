dccc7feaa516d1440d17b4b55221b8b8a140ac5c
Lisa <lb.broadhead@gmail.com>
2022-03-12 15:24:27 -0600

    box generator

---
+++ react/to-do/src/components/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from './css/ToDoStyles.css';
+
+const ToDo = props => {
+    return (
+        <div className={ styled.listWrp }>
+            
+        </div>
+    );
+}
+
+export default ToDo;
