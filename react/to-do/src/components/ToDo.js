85884c4fee3e95d19bd46cc0e36a9b05af02e848
Lisa <lb.broadhead@gmail.com>
2022-03-13 08:53:55 -0500

    box generator

--- react/to-do/src/components/ToDo.js
+++ react/to-do/src/components/ToDo.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import styled from './css/ToDoStyles.css';
 
 const ToDo = props => {
+    const [newItem, setNewItem] = useState({
+            task:"",
+            msg:"",
+            completed: false
+        })
+
+    const onChangeHandler = (event) => {
+        event.target.value.length < 4 ? setNewItem({})
+    }
+
+    const sumbitNewTask = () => {
+
+    }
+
     return (
         <div className={ styled.listWrp }>
-            
+            <label htmlFor="newTask">New To Do Item:</label>
+            <input type="text" name="newTask" onChange = { onChangeHandler } />
+
+            <input type="submit" value="submit" onClick = { sumbitNewTask} />
         </div>
     );
 }
