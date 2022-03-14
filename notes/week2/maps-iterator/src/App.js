21ad023ee390d2a6ebfe078c2c2f5783fbf690f8
Lisa <lb.broadhead@gmail.com>
2022-03-14 10:43:38 -0500

    morning lecture

--- notes/week2/maps-iterator/src/App.js
+++ notes/week2/maps-iterator/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [places,setPlaces] = useState([
+    "🍨 Mason's Creamy",
+    "🍨  Mitchell's Ice Cream",
+    "🍦 Dairy Queen",
+    "🍨 Swan Valley Creamy",
+    "🍨 Inova",
+    "🍦 Cold Stone",
+    "🍨 Homemade Ice Cream",
+    "🍦 Baskin Robins",
+    "🍦 TCBY"
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ICE CREEEEEEEEEEAM!</h1>
+      <p>Favorite Ice Cream location</p>
+      <div>
+        {
+          places.map( (item, i) => <p key={ i }>{ item }</p> ) 
+        }
+        <ol>
+        {
+          places.map((item,i) => <li key={i}> { item } </li> )
+        }
+        </ol>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// no for loops in your return statements
+// mao is a function that takes a funtion as a parameter
+//map needs to have JSX (html) tags
