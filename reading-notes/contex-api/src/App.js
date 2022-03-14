acc87d59526b109a2fcd7d8eb17ab21d61ffa4dc
Lisa <lb.broadhead@gmail.com>
2022-03-14 15:03:24 -0500

    more notes

--- reading-notes/contex-api/src/App.js
+++ reading-notes/contex-api/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyContext from './context/MyContext';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How weird are you?</h1>
+      <p>Have you ever / would you ever?</p>
+      <MyContext.Provider />
     </div>
   );
 }
---
+++ reading-notes/contex-api/src/context/MyContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyContext = props => {
+    return(
+        <div>
+            <h2>Context provider</h2>
+        </div>
+    );
+}
+
+export default MyContext;
