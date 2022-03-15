ed850e8247ee6105d6a5ea1c520e6071ac6199be
Lisa <lb.broadhead@gmail.com>
2022-03-15 12:47:32 -0500

    coin toss

--- react/coin-flip/src/App.js
+++ react/coin-flip/src/App.js
@@ -4,36 +4,35 @@ import { useState } from 'react';
 function App() {
   const [coin,setCoin] = useState({
     headsCount: 0,
-    attempts: 0
+    attempts: 95,
+    message: ""
   })
 
-const tossCoin = () => { return Math.random() > 0.5 ? "heads" : "tails"} ;
-    
-function fiveHeads() {
-  return new Promise( (resolve, reject) => {
-    let headsCount = 0;
-    let attempts = 0;
+  const tossCoin = () => { return Math.random() > 0.5 ? "heads" : "tails"} ;
       
-    while(headsCount < 5) {
-      attempts++;
-      let result = tossCoin();
+  const fiveHeads = (event) => {
+    return new Promise( (resolve, reject) => {
+          console.log(coin.headsCount)
+          while(coin.headsCount < 5 && coin.attempts < 100) {
+            setCoin({...coin, attempts: coin.attempts + 1});
+            let result = tossCoin();
 
-      console.log(`${result} was flipped`);
-      if(result === "heads"){
-          headsCount++;
-      } else {
-          headsCount = 0;
-      }
+            console.log(`${result} was flipped`);
+            if(result === "heads"){
+              setCoin({...coin, headsCount: coin.headsCount + 1});
+            } else {
+              setCoin({...coin, headsCount: 0});
+            }
+          }//end while
 
-      if(headsCount === 5) {
-        resolve("5 heads in a row!");
-      } else if(attempts > 100) {
-        reject("You've hit your limit");
-      }
+        if(coin.headsCount === 5) {
+            resolve("Good, it's not Monday!");
+        } else {
+            reject("Someone has a case of the Mondays!");
+        }
 
-    }
-  });
-}
+    });
+  }
 
   fiveHeads()
   .then( res => console.log(res) ) 
@@ -46,7 +45,10 @@ function fiveHeads() {
       <div className="container">
         <h1>Money Money Money!</h1>
         <p>Flip the coin...you may win a million dollars!</p>
-
+        <button onClick={fiveHeads}>Flip a Coin</button>
+        <p className="message">{coin.message}</p>
+        <p>Attempts: {coin.attempts} </p>
+        <p>Head Count: {coin.headsCount} </p>
       </div>  
       <div className="background"></div>
     </div>
