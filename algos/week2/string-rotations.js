c17a45c7d0b5c6ca9c3e6b34a6c79f0fac7aa08d
Lisa <lb.broadhead@gmail.com>
2022-03-15 10:40:44 -0500

    coin toss

---
+++ algos/week2/string-rotations.js
@@ -0,0 +1,62 @@
+//Create a standalone function that accepts a string
+//and an integer, and rotates the characters in the
+//string to the right by that amount.
+//this one must be done without built in methods
+//("Did I shine my shoes today?", 9) ->
+//"es today?Did I shine my sho"
+const rotateString = (str, num) => {
+    var newString = "";
+    var rotate = str.length - 1;
+
+    for(let x = 0; x < str.length ;x++){
+        if(x >= str.length - num){
+            newString = str[rotate] + newString;
+            rotate--;
+        }else{
+            newString += str[x];
+        }
+    }
+    return newString;
+}
+
+// let testString = "012345";
+// console.log(rotateString(testString, 3));
+
+// let shoes = "Did I shine my shoes today?";
+// console.log(rotateString(shoes, 9));
+
+//write a function that will return true if str2 is a
+//rotation of str1. otherwise return false
+//("Did I shine my shoes today?", "es today?Did I shine my shoe")
+// -> returns true
+const isRotation = (str1, str2,num) => {
+    var rotate = 0;
+    var same = false;
+
+    if(str1.length === str2.length){
+        if(num){    
+            rotate = num
+            str1 = rotateString(str1,rotate);
+        }
+
+        for(x=0;x < str2.length; x++){
+            if(str2[x] === str1[x]){
+                same = true;
+                continue;
+            }else{
+                same = false;
+                if(rotate + 1 < str2.length - 1){
+                    rotate++;
+                    isRotation(str1,str2,rotate)
+                    break;
+                }else{
+                    return same;
+                }
+            }
+        }
+    }
+    return same;
+}
+
+console.log(isRotation("es today?Did I shine my sho", "es today?Did I shine my sho"));
+// console.log(isRotation("Did I shine my shoes today? ", "es today?Did I shine my sho"));
--- algos/week2/tempCodeRunnerFile.js
+++ algos/week2/tempCodeRunnerFile.js
@@ -1,1 +1,6 @@
-f(str[i] === " "){
+if(rotate + 1 < str2.length -1){
+            //     console.log(str2[x] + " : "+ newStr[x])
+            //     rotate++;
+            //     isRotation(newStr,str2,rotate)
+            // }else{
+            // }
