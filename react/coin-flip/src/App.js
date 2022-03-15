6077f9911433ba1a618adeae271372fa48322551
Lisa <lb.broadhead@gmail.com>
2022-03-15 12:59:11 -0500

    coin toss

--- react/coin-flip/src/App.js
+++ react/coin-flip/src/App.js
@@ -4,49 +4,49 @@ import { useState } from 'react';
 function App() {
   const [coin,setCoin] = useState({
     headsCount: 0,
-    attempts: 95,
-    message: ""
+    attempts: 0
   })
 
-  const tossCoin = () => { return Math.random() > 0.5 ? "heads" : "tails"} ;
+const tossCoin = () => { return Math.random() > 0.5 ? "heads" : "tails"} ;
+    
+function fiveHeads() {
+  return new Promise( (resolve, reject) => {
+    let headsCount = 0;
+    let attempts = 0;
       
-  const fiveHeads = (event) => {
-    return new Promise( (resolve, reject) => {
-          while(coin.headsCount < 5 && coin.attempts < 100) {
-            setCoin({...coin, attempts: coin.attempts + 1});
-            let result = tossCoin();
+    while(headsCount < 5) {
+      attempts++;
+      let result = tossCoin();
 
-          //   console.log(`${result} was flipped`);
-            if(result === "heads"){
-              setCoin({...coin, headsCount: coin.headsCount + 1});
-            } else {
-              setCoin({...coin, headsCount: 0});
-            }
-          // }//end while
+      console.log(`${result} was flipped`);
+      if(result === "heads"){
+          headsCount++;
+      } else {
+          headsCount = 0;
+      }
 
-        // if(coin.headsCount === 5) {
-        //   setCoin({...coin, message: "You won! You won! You won!"});
-        //   resolve("You won! You won! You won!");
-        // } else {
-        //   setCoin({...coin, message: "Waa Waa Waaa...You now owe me 1 MILLION dollars!"});
-        //   reject("Waa Waa Waaa...I won! I won! I won! You owe me $1 MILLION!");
-        }
-    });
-  }
+      if(headsCount === 5) {
+        resolve("5 heads in a row!");
+      } else if(attempts > 100) {
+        reject("You've hit your limit");
+      }
+
+    }
+  });
+}
 
   fiveHeads()
   .then( res => console.log(res) ) 
   .catch( err => console.log(err) );
+
+  console.log( "When does this run now?" );
     
   return (
     <div className="App">
       <div className="container">
         <h1>Money Money Money!</h1>
         <p>Flip the coin...you may win a million dollars!</p>
-        <button onClick={fiveHeads}>Flip a Coin</button>
-        <p className="message">{coin.message}</p>
-        <p>Attempts: {coin.attempts} </p>
-        <p>Head Count: {coin.headsCount} </p>
+
       </div>  
       <div className="background"></div>
     </div>
