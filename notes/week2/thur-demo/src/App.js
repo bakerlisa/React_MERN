11894ab27137d2c538717ea9579f31a81e17d571
Lisa <lb.broadhead@gmail.com>
2022-03-16 14:13:41 -0500

    thir lecture

--- notes/week2/thur-demo/src/App.js
+++ notes/week2/thur-demo/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './views/home';
+import Second from './views/Second';
+import { Switch, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/"  >
+          <Home/>
+        </Route>
+      </Switch>
+      <Second/>
     </div>
   );
 }
---
+++ notes/week2/thur-demo/src/views/Second.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Second = props =>{
+    return(
+<h1><marquee behavior="" direction="">Its me</marquee></h1>
+    );
+}
+
+export default Second;
