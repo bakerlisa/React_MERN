72206568f0f327e6a37b4b7fa25b2e37af39040a
Lisa <lb.broadhead@gmail.com>
2022-03-18 17:19:55 -0500

    practice

--- express/morePractice/server.js
+++ express/morePractice/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
-const express = require("express");
-const app = express();
-const port = 8000;
     
 // req is shorthand for request
 // res is shorthand for response
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
+    res.json({ message: "Hello Lisa" });
 });
 
 // this needs to below the other code blocks
