9d49f34110a654ddbdd17cf1964c4577c4817b2f
Lisa <lb.broadhead@gmail.com>
2022-03-18 12:00:08 -0500

    practice

--- express/EXPRESS-INTRO/server.js
+++ express/EXPRESS-INTRO/server.js
@@ -1,5 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.listen(port, ()=>console.log(`running on port ${port} in a new way`);
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
