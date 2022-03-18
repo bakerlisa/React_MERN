9672ba7a6e7f2fa92c064552574e5a32d4bdd165
Lisa <lb.broadhead@gmail.com>
2022-03-17 19:07:13 -0500

    practice

--- practice-makes-perfect/moreforms/src/App.js
+++ practice-makes-perfect/moreforms/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch,Route,Link } from 'react-router-dom';
+import Home from './components/views/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
---
+++ practice-makes-perfect/moreforms/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = (props) =>{
+    return(
+        <div>
+            <h2>form</h2>
+        </div>
+    );
+}
+
+export default Form;
