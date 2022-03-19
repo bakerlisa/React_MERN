2314aa443c7638922333034829da43a545164b3b
Lisa <lb.broadhead@gmail.com>
2022-03-19 18:19:53 -0500

    practice

--- practice-makes-perfect/fridayPractice/IceCream.js
+++ practice-makes-perfect/fridayPractice/IceCream.js
@@ -1,7 +1,16 @@
 const { faker } = require('@faker-js/faker');
 
 class IceCream{
+    constructor(){
+        const toppings = ["whipped cream", "jelly beans", "mashmellows","hot fudge","carmel","sprinkles","rainbows","Cookie crumbs","cinnamon","honey"]
 
+        this.toppings = toppings[Math.floor(Math.random() * toppings.length)]
+        this.name = faker.name.findName();
+        this.animal = faker.animal.dog();
+        this.hacker = faker.hacker.noun();
+        this.helpers = faker.helpers.createTransaction()
+        this.job = faker.name.jobArea();
+    }
 }
 
 module.exports = IceCream;
--- practice-makes-perfect/fridayPractice/server.js
+++ practice-makes-perfect/fridayPractice/server.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 8000;
 const { faker } = require('@faker-js/faker');
 
-const IceCream = require("./companies");
+const IceCream = require("./iceCream");
     
 // req is shorthand for request
 // res is shorthand for response
 app.get("/api", (req, res) => {
     res.json({ message: "Hello World" });
 });
+
+app.get("/api/orders", (req,res) => {
+    res.json(new IceCream());
+})
 
 app.listen( port, () => console.log(`Listening on port: ${port} Hell Yeah!`) );
