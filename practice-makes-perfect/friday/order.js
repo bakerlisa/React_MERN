4d0dbc902bc954485898bba72585077dca43e53d
Lisa <lb.broadhead@gmail.com>
2022-03-20 17:33:33 -0500

    more practice

---
+++ practice-makes-perfect/friday/order.js
@@ -0,0 +1,8 @@
+class Order{
+    constructor(){
+        this.name = "Lisa"
+        this.last = "Broadhead"
+    }
+}
+
+module.exports = Order;
--- practice-makes-perfect/friday/server.js
+++ practice-makes-perfect/friday/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const Order = require("./order");
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello World" });
 });
+
+app.get("/api/order", (req,res) => {
+    res.json(new Order())
+})
 
 app.listen( port, () => console.log(`Listening on port: ${port} Hell Yeah!`) );
