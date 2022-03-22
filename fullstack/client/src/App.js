70af5c1751f0c1b67b85b65cc7b36cb21ffc43ac
Lisa <lb.broadhead@gmail.com>
2022-03-22 17:23:19 -0500

    fullstack

--- fullstack/client/src/App.js
+++ fullstack/client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Switch,Route,useHistory } from 'react-router-dom';
+
+import Starship from './components/views/Form';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Switch>
+        <Route>
+          <Form />
+        </Route>
+      </Switch>
     </div>
   );
 }
