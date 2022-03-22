f999c6492ca35b36523c9718bb4571f7321f9c4b
Lisa <lb.broadhead@gmail.com>
2022-03-22 11:05:23 -0500

    algos

--- notes/week3/mongoose/backend/server/models/sundea.model.js
+++ notes/week3/mongoose/backend/server/models/sundea.model.js
@@ -1,12 +1,15 @@
 import mongoose = require("mongoose");
 
 const SundeasSchema = new mongoose.Schema({
-    flavor: String,
+    flavor: {
+        type: String,
+        requires: [true, "You need an ice cream flavor"]
+    },
     numScoops: Number,
     whipped: Boolean,
     name: String,
     toppings: [String]
-})
+},{timestamps:true})
 
 const Sundea = Mongoose.model("Sundea",SundeasSchema);
 
