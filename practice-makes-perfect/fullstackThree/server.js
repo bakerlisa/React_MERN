629895f6974ba42d8088ee8d95334fd6360c3284
Lisa <lb.broadhead@gmail.com>
2022-03-22 14:16:17 -0500

    fullstack

--- practice-makes-perfect/fullstackThree/server.js
+++ practice-makes-perfect/fullstackThree/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors') // This is new
 const app = express();
 app.use(cors()) // This is new
-require('./server/routes/cat.route')(app);
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
 
+require("./server/config/mongoose.config");
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+require('./server/routes/cat.route')(app);
+    
+app.listen(8000, () => console.log("The server is all fired up on port 8000 Hell Yeah"));
--- practice-makes-perfect/fullstackThree/server/controllers/cat.controller.js
+++ practice-makes-perfect/fullstackThree/server/controllers/cat.controller.js
@@ -1,7 +1,7 @@
-const Cat = require('../models/car.model');
+const Cat = require('../models/cat.model');
 
 module.exports.findAllCats = (req, res) => {
     Cat.find()
-        .then(allDaCats => res.json({ users: allDaCats }))
+        .then(allDaCats => res.json({ cats: allDaCats }))
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
