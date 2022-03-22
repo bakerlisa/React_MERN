ed09875b5fa11e1191b999ac450e76e05e971c74
Lisa <lb.broadhead@gmail.com>
2022-03-22 16:25:39 -0500

    fullstack

--- notes/week3/mongoose/backend/server/config/mongoose.cofig.js
+++ notes/week3/mongoose/backend/server/config/mongoose.cofig.js
@@ -4,5 +4,5 @@ mongoose.connect("mongodb://localhost/march_2022_sundeas", {
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
-    .then(() =>console.log("Hey Dude you're connected to the freakin' database!"))
-    .catch(err=>console.log("beep beep beep db connection was a flop",err))
+    .then(() => console.log("Hey Dude you're connected to the freakin' database!"))
+    .catch(err=> console.log("beep beep beep db connection was a flop",err))
