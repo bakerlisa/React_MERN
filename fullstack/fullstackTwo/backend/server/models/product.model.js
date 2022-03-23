c6af6ad6140d88030569fb908e61de3d308f478c
Lisa <lb.broadhead@gmail.com>
2022-03-22 21:32:59 -0500

    fullstack

--- fullstack/fullstackTwo/backend/server/models/product.model.js
+++ fullstack/fullstackTwo/backend/server/models/product.model.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
+    title: {
+        type:String,
+        required: [true, "Title is required"],
+        minlength: [3, "Title needs to be at least 3 characters"]
+    },
+    price: {
+        type:Number,
+        required: [true, "Product needs a price"],
+        minlength: [1, "Product must be at least 1 number long"]
+    },
+    description: {
+        type:String,
+        required: [true, "Your product needs a description"],
+        minlength: [true, "You must have a minimum of 10 chacters"]
+    },
+    amount:{
+        type:Number
+    },
+    amountType:{
+        type: String
+    }
+},{timestamps:true});
 
 const Product = mongoose.model('Product', ProductSchema);
 
--- fullstack/fullstackTwo/backend/server/routes/product.route.js
+++ fullstack/fullstackTwo/backend/server/routes/product.route.js
@@ -1,5 +1,8 @@
-const UserController = require('../controllers/product.controller');
+const ProductController = require('../controllers/product.controller');
 
 module.exports = app => {
     app.get('/api/products', ProductController.findAllProducts);
+    app.get('/api/product/:id', ProductController.singleProduct);
+    app.post('/api/create/product', ProductController.createNewProduct);
+    app.delete('/api/delete/product/:id', ProductController.deleteProduct);
 }
