8155e308e9a5c4d8b097c0b3cbcbeeccfcf0c7bd
Lisa <lb.broadhead@gmail.com>
2022-03-22 21:55:53 -0500

    fullstack

--- fullstack/fullstackThree/client/src/App.js
+++ fullstack/fullstackThree/client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Switch,Route,useHistory,Link } from 'react-router-dom';
+
+import AddProductForm from './components/views/AddProductForm';
+import Home from './components/views/Home';
+import SingleProduct from './components/views/SingleProduct';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/addProduct">Creat Product</Link>
+      </nav>
+      
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route exact path="/addProduct">
+            <AddProductForm />
+          </Route>
+
+          <Route exact path="/:id">
+            <SingleProduct />
+          </Route>
+
+        </Switch>
+
+      <footer>
+        Footer
+      </footer>
     </div>
   );
 }
