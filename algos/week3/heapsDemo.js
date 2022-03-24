1e1b48012822f249305cf759606087b59bf5b99f
Lisa <lb.broadhead@gmail.com>
2022-03-24 10:15:30 -0500

    fullstack

--- algos/week3/heapsDemo.js
+++ algos/week3/heapsDemo.js
@@ -25,6 +25,52 @@ class MinHeap{
     min(){
         return this.heap.length < 2 ? null : this.heap[1];
     }
+
+    remove(){
+        //empty
+        if(this.heap.length < 2) return null;
+        
+        //one value
+        const min = this.heap[1];
+
+        if(this.heap.length === 2){
+            this.heap.pop();
+            return min;
+        }
+        // multiple values left
+
+
+        // always returning min
+        this.heap[1] = this.heap.pop();
+        this.shiftDown();
+        return min;
+    }
+
+    shiftDown(){
+        let indToShiftDown  = 1;
+        let leftIndx = indToShiftDown * 2;
+
+        while(leftIndx < this.heap.length){
+            let rightIndx = leftIndx + 1;
+            let indxOfSmallest =  leftIndx;
+
+            if(rightInx < this.heap.length && this.heap[rightIndx] < this.heap[leftIndx]){
+                let indxOfSmallest =  rightIndx;
+            }
+
+            if(this.heap[indToShiftDown] <= this.heap[indxOfSmallest]){
+                console.log("value has found its home")
+                break;
+            }else{
+                var temp = this.heap[indToShiftDown]
+                this.heap[indToShiftDown] = this.heap.indxOfSmallest;
+                this.heap.indxOfSmallest = temp;
+                indToShiftDown = indxOfSmallest;
+                leftIndx = indToShiftDown *2;
+            }
+
+        }
+    }
 }
 
 const myHeap = new MinHeap();
