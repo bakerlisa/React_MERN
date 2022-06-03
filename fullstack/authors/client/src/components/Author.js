2a807947b4661f92a43b052a6408744620374fd2
Lisa <lb.broadhead@gmail.com>
2022-06-03 07:54:58 -0500

    first commit

--- fullstack/authors/client/src/components/Author.js
+++ fullstack/authors/client/src/components/Author.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from '../css/AuthorStyles.module.css'
 import Like from './Like';
+import sanderson from '../img/sanderson.png'
+import iilsa from '../img/iilsa.png'
 
 const Author = (props) => {
     const [books,setBooks] = useState(props.author.books)
     const [genre,setGenre] = useState(props.author.genre)
+    const [img,setImg] = useState("")
 
+    
+    useEffect(() => {
+        if(props.author.author === "Brandon Sanderson"){
+            setImg({sanderson});
+        }else if(props.author.author === "Ilsa J. Beck "){
+            setImg({iilsa});
+        }
+    })
     return(
-        <div className={styled.authorBox}>
-            <img className={styled.authorImg} src={props.author.image} alt={props.author.author} />
+        <div className={styled.authorBox}> 
+            <div className={styled.imgWrp}>{
+                img.length > 0 ? <img className={styled.authorImg} src={img} alt={props.author.author} /> : <img className={styled.authorImg} src={props.author.image} alt={props.author.author} />
+            }</div>
+            
             <Link to={`/edit/${props.author._id}`} className={styled.name}>{props.author.author}</Link>
 
             <ul className={styled.booksGenreList}>
